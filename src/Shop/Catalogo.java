aec5a0f24ed363e64108470ae3f0d271b6e52d2d
federicacetroni <cetronifederica97@gmail.com>
2022-06-22 17:45:57 +0200

    corretta classe catalogo

--- src/Shop/Catalogo.java
+++ src/Shop/Catalogo.java
@@ -11,83 +11,78 @@ public class Catalogo {
 		Prodotto[] catalogo = new Prodotto[3];
 
 		for (int i = 0; i < catalogo.length; i++) {
-			{
-				System.out.println("Il nostro shop offre i seguenti prodotti: televisori, smartphone e cuffie!");
-				System.out.println("Che prodotto vuoi inserire?");
-				String scelta = scan.nextLine();
 
-				switch (scelta) {
+			System.out.println("Il nostro shop offre i seguenti prodotti: televisori, smartphone e cuffie!");
+			System.out.println("Che prodotto vuoi inserire?");
+			System.out.println("Inserisci 1 per il televisore, 2 per lo smartphone, 3 per le cuffie");
+			int scelta = scan.nextInt();
 
-				case "Televisori":
-					System.out.println("Inserisci il nome: ");
-					String nome = scan.nextLine();
-					System.out.println("Inserisci la marca: ");
-					String marca = scan.nextLine();
-					System.out.println("Inserisci la tipologia, se smart tv o no: ");
-					String tipologia = scan.nextLine();
-					System.out.println("Inserisci le dimensioni: ");
-					int dimensioni = scan.nextInt();
-					System.out.println("Inserisci il prezzo: ");
-					double prezzo = scan.nextDouble();
-					System.out.println("Inserisci l'iva: ");
-					int iva = scan.nextInt();
+			if (scelta == 1) {
 
-					Televisori televisori = new Televisori(nome, marca, prezzo, iva, 0, dimensioni, tipologia);
-					televisori.getCodice();
-					System.out.println("Prodotto inserito: " + televisori.toString());
-					break;
+				System.out.println("Inserisci il nome: ");
+				String nome = scan.nextLine();
+				System.out.println("Inserisci la marca: ");
+				String marca = scan.nextLine();
+				System.out.println("Inserisci la tipologia, se smart tv o no: ");
+				String tipologia = scan.nextLine();
+				System.out.println("Inserisci le dimensioni: ");
+				int dimensioni = scan.nextInt();
+				System.out.println("Inserisci il prezzo: ");
+				double prezzo = scan.nextDouble();
+				System.out.println("Inserisci l'iva: ");
+				int iva = scan.nextInt();
 
-				case "Smartphone":
-					System.out.println("Inserisci il nome: ");
-					String nomeSmart = scan.nextLine();
-					System.out.println("Inserisci la marca: ");
-					String marcaSmart = scan.nextLine();
-					System.out.println("Inserisci la memoria: ");
-					int memoriaSmart = scan.nextInt();
-					System.out.println("Inserisci il prezzo: ");
-					double prezzoSmart = scan.nextDouble();
-					System.out.println("Inserisci l'iva: ");
-					int ivaSmart = scan.nextInt();
+				Televisori televisori = new Televisori(nome, marca, prezzo, iva, 0, dimensioni, tipologia);
+				televisori.getCodice();
+				System.out.println("Prodotto inserito: " + televisori.toString());
+			} else if (scelta == 2) {
+				System.out.println("Inserisci il nome: ");
+				String nomeSmart = scan.nextLine();
+				System.out.println("Inserisci la marca: ");
+				String marcaSmart = scan.nextLine();
+				System.out.println("Inserisci la memoria: ");
+				int memoriaSmart = scan.nextInt();
+				System.out.println("Inserisci il prezzo: ");
+				double prezzoSmart = scan.nextDouble();
+				System.out.println("Inserisci l'iva: ");
+				int ivaSmart = scan.nextInt();
 
-					Smartphone smartphone = new Smartphone(nomeSmart, marcaSmart, prezzoSmart, ivaSmart, 0, 0,
-							memoriaSmart);
-					smartphone.getCodice();
-					smartphone.getImei();
-					System.out.println("Prodotto inserito: " + smartphone.toString());
-					break;
+				Smartphone smartphone = new Smartphone(nomeSmart, marcaSmart, prezzoSmart, ivaSmart, 0, 0,
+						memoriaSmart);
+				smartphone.getCodice();
+				smartphone.getImei();
+				System.out.println("Prodotto inserito: " + smartphone.toString());
 
-				case "Cuffie":
-					System.out.println("Inserisci il nome: ");
-					String nomeCuffie = scan.nextLine();
-					System.out.println("Inserisci la marca: ");
-					String marcaCuffie = scan.nextLine();
-					System.out.println("Inserisci il colore:  ");
-					String colore = scan.nextLine();
-					System.out.println("Inserisci la tipologia, se wireless o cablate: ");
-					String tipologiaCuffie = scan.nextLine();
-					System.out.println("Inserisci il prezzo: ");
-					double prezzoCuffie = scan.nextDouble();
-					System.out.println("Inserisci l'iva: ");
-					int ivaCuffie = scan.nextInt();
+			} else if (scelta == 3) {
+				System.out.println("Inserisci il nome: ");
+				String nomeCuffie = scan.nextLine();
+				System.out.println("Inserisci la marca: ");
+				String marcaCuffie = scan.nextLine();
+				System.out.println("Inserisci il colore:  ");
+				String colore = scan.nextLine();
+				System.out.println("Inserisci la tipologia, se wireless o cablate: ");
+				String tipologiaCuffie = scan.nextLine();
+				System.out.println("Inserisci il prezzo: ");
+				double prezzoCuffie = scan.nextDouble();
+				System.out.println("Inserisci l'iva: ");
+				int ivaCuffie = scan.nextInt();
 
-					Cuffie cuffie = new Cuffie(nomeCuffie, marcaCuffie, prezzoCuffie, ivaCuffie, 0, colore,
-							tipologiaCuffie);
-					cuffie.getCodice();
-					System.out.println("Prodotto inserito: " + cuffie.toString());
-					break;
-
-				}
+				Cuffie cuffie = new Cuffie(nomeCuffie, marcaCuffie, prezzoCuffie, ivaCuffie, 0, colore,
+						tipologiaCuffie);
+				cuffie.getCodice();
+				System.out.println("Prodotto inserito: " + cuffie.toString());
 
 			}
 
-			/*
-			 * System.out.println("Il nostro catalogo: "); String intestazione =
-			 * String.format("%15s %15s %15s", "Marca", "Nome", "Prezzo");
-			 * System.out.println(intestazione); for (int i = 0; i < catalogo.length; i++) {
-			 * String infoProdotto = String.format("%15s %15s %15s", catalogo[i].marca,
-			 * catalogo[i].nome, catalogo[i].prezzo); System.out.println(infoProdotto);
-			 */
 		}
+
+		/*
+		 * System.out.println("Il nostro catalogo: "); String intestazione =
+		 * String.format("%15s %15s %15s", "Marca", "Nome", "Prezzo");
+		 * System.out.println(intestazione); for (int i = 0; i < catalogo.length; i++) {
+		 * String infoProdotto = String.format("%15s %15s %15s", catalogo[i].marca,
+		 * catalogo[i].nome, catalogo[i].prezzo); System.out.println(infoProdotto);
+		 */
 
 		scan.close();
 	}
